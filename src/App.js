254eb33779d03b8254b3025005158c217bd9aec4
3216workbench <hubdavin@gmail.com>
2025-07-24 20:12:51 +0100

    container def

--- src/App.js
+++ src/App.js
@@ -5,19 +5,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          This is a prototype of cardle app
+        </p> 
       </header>
+      <main>
+        <div>
+          <p>
+            photo
+          </p>
+        </div>
+        <div>
+          <p>
+            guess check status
+          </p>
+        </div>
+        <div>
+          <p>
+            form input
+          </p>
+        </div>
+      </main>
+      <footer></footer>
     </div>
   );
 }
